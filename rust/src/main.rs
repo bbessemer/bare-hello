e2b3b4a626a3693e5dd0c117a9cbe0341483938d
Brent Bessemer <bmb0610@gmail.com>
2020-10-01 00:30:59 -0500

    Add basic rust example

---
+++ rust/src/main.rs
@@ -0,0 +1,75 @@
+#![no_std]
+#![no_main]
+
+#![feature(asm)]
+#![feature(naked_functions)]
+
+use core::panic::PanicInfo;
+use core::slice;
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+const MB_MAGIC: i32 = 0x1badb002;
+const MB_FLAGS: i32 = 3;
+const MB_CHECKSUM: i32 = -(MB_MAGIC + MB_FLAGS);
+
+struct MultibootHeader {
+    _magic: i32,
+    _flags: i32,
+    _checksum: i32,
+}
+
+#[no_mangle]
+#[link_section = ".multiboot"]
+static multiboot_header: MultibootHeader = MultibootHeader {
+    _magic: MB_MAGIC,
+    _flags: MB_FLAGS,
+    _checksum: MB_CHECKSUM,
+};
+
+struct Vga {
+    buf: &'static mut [u16],
+}
+
+impl Vga {
+    pub fn new() -> Self {
+        Vga {
+            buf: unsafe {
+                slice::from_raw_parts_mut(0xb8000 as *mut u16, 80*25)
+            },
+        }
+    }
+
+    pub fn print(&mut self, msg: &[u8], color: u16) {
+        for (dst, src) in self.buf.iter_mut().zip(msg.iter()) {
+            *dst = (*src as u16) | (color << 8);
+        }
+        for dst in &mut self.buf[msg.len()..] {
+            *dst = color << 8;
+        }
+    }
+}
+
+#[no_mangle]
+#[naked]
+pub unsafe extern "C" fn _start() -> ! {
+    asm!("mov esp, {}", const 0xf00000);
+    
+    let mut vga = Vga::new();
+    vga.print(b"Hello, world!", 0x07);
+
+    loop {
+        asm!("hlt");
+    }
+}
+
+// rustc expects this to exist
+#[no_mangle]
+pub unsafe extern "C" fn memcpy(dest: *mut u8, src: *const u8, len: usize) {
+    for i in 0..len {
+        *dest.offset(i as isize) = *src.offset(i as isize);
+    }
+}
